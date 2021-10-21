c9eb0ed829e4bbefcbeeb8030354e7b4bb0acb1c
Marvin Kuhn <kuhn@wysiwyg.de>
2021-10-21 10:07:25 +0200

    feat: add custom fusion prototype for filter array

---
+++ Classes/FusionObjects/ListFilterImplementation.php
@@ -0,0 +1,8 @@
+<?php
+namespace Breadlesscode\Listable\FusionObjects;
+
+use Neos\Fusion\FusionObjects\DataStructureImplementation;
+
+class ListFilterImplementation extends DataStructureImplementation
+{
+}
